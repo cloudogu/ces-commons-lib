7b1f6d57043093a93f6440e6064ab1095192e60a
Niklas Roeske <niklas.roeske@cloudogu.com>
2024-10-25 14:48:28 +0200

    #1 RemoteDoguDescriptorRepository interface and errors

--- dogu/interfaces.go
+++ dogu/interfaces.go
@@ -1,6 +1,8 @@
 package dogu
 
 import (
+	"context"
+	"errors"
 	"github.com/cloudogu/cesapp-lib/core"
 )
 
@@ -14,4 +16,20 @@ type QualifiedDoguVersion struct {
 type QualifiedDoguName struct {
 	SimpleName SimpleDoguName
 	Namespace  DoguNamespace
+}
+
+var ErrDoguDescriptorNotFound = errors.New("No DoguDescriptor found for that dogu")
+var ConnectionError = errors.New("There are some connection issues")
+
+type RemoteDoguDescriptorRepository interface {
+	// GetLatest returns the dogu descriptor for a dogu from the remote server.
+	// NotFoundError if there is no descriptor for that dogu
+	// ConnectionError if there are any connection issues
+	// Generic Error if there are any other issues
+	GetLatest(context.Context, QualifiedDoguName) (*core.Dogu, error)
+	// Get returns a version specific dogu descriptor.
+	// NotFoundError if there is no descriptor for that dogu
+	// ConnectionError if there are any connection issues
+	// Generic Error if there are any other issues
+	Get(context.Context, QualifiedDoguVersion) (*core.Dogu, error)
 }
